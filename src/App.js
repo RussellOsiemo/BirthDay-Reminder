4f60c62a0725e026586122e830fa5b1f93bb64c2
Russel Osiemo <86182360+RussellOsiemo@users.noreply.github.com>
2021-10-25 14:33:29 +0300

    starter style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+// import './App.css';
+ import './index.css';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>My Friends Birthday reminder</h1>
       </header>
+      <div className="container">
+       <div className="item">
+         {/* <h4 className="work"> Teacher</h4>
+         <h6>27 October 2002</h6>
+         <span>In Three Days</span>
+         <br />
+         <span className="name">
+           Gayla Fahnkerry
+         </span>
+         <p className="wish">"I wish to Have more friends this year AND OF COURSE THE MONEY!!!"</p> */}
+       </div>
+      </div>
     </div>
   );
 }
