c405d0124b3f27eeacd77e710929a009d79626ea
Russel Osiemo <86182360+RussellOsiemo@users.noreply.github.com>
2021-10-27 08:52:10 +0300

    bootstrap import

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 // import logo from './logo.svg';
 // import './App.css';
  import './index.css';
+ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <div className="App">
+  
+       <div className="App">
       <header className="App-header">
        <h1>My Friends Birthday reminder</h1>
       </header>
-      <div className="container">
+       <section className="row">
+    
+     
+      <div className="col-md-6">
        <div className="item">
-         {/* <h4 className="work"> Teacher</h4>
+          <h4 className="work"> Teacher</h4>
          <h6>27 October 2002</h6>
          <span>In Three Days</span>
          <br />
          <span className="name">
            Gayla Fahnkerry
          </span>
-         <p className="wish">"I wish to Have more friends this year AND OF COURSE THE MONEY!!!"</p> */}
+         <p className="wish">"I wish to Have more friends this year AND OF COURSE THE MONEY!!!"</p> 
+         <button className="btn-info">Remove</button>
        </div>
       </div>
-    </div>
+    
+     </section>
+     </div>
   );
 }
 
