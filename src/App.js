ed9a19e286dd6127d3c974e64fba1bd0a7f5e3b0
Russel Osiemo <86182360+RussellOsiemo@users.noreply.github.com>
2021-10-28 22:24:34 +0300

    bootstraping

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@
 // import './App.css';
  import './index.css';
  import "bootstrap/dist/css/bootstrap.min.css";
+  var today = new Date();
+  //date im counting down to
+ var bDay = new Date("Dec 27, 2021 00.00.00").getTime(); 
+//update the count down every one second
+var x = setInterval(() => {
+  //get todays time and date
+  var now = new Date().getTime();
+  //find the time from now to the Birthday
+  var interval = endDate - now;
+  //time calculation for days hour minutes and seconds
+  var days = Math.floor(interval/ (1000 *60*60*24));
+  var hours = Math.floor(interval % (1000*60*60*24))
+}, interval);
 function App() {
   return (
   
@@ -12,17 +25,20 @@ function App() {
        <section className="row">
     
      
-      <div className="col-md-6">
-       <div className="item">
-          <h4 className="work"> Teacher</h4>
-         <h6>27 October 2002</h6>
-         <span>In Three Days</span>
-         <br />
-         <span className="name">
+      <div className="col-md-9 d-flex justify-content-center">
+       <div className="card"><h2 className="name">
            Gayla Fahnkerry
-         </span>
-         <p className="wish">"I wish to Have more friends this year AND OF COURSE THE MONEY!!!"</p> 
-         <button className="btn-info">Remove</button>
+         </h2>
+          <h4 className="work"> Teacher</h4>
+           <img src={"https://images.unsplash.com/photo-1488426862026-3ee34a7d66df?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d29tYW58ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60"} className="dp" alt="profile"/>
+         <h6>27 October 2002</h6>
+         <h6 className='justify-content-right'>one month</h6>
+         <br />
+        
+         
+         
+         <p className="mx-auto">"I wish to Have more friends this year AND OF COURSE THE MONEY!!!"</p> 
+         <button type='button' className="btn btn-outline-warning">Remove</button>
        </div>
       </div>
     
